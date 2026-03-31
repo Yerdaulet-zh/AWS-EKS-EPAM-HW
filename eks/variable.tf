58b4e684aa3b667749e337648549d263221a9365
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-03-31 17:09:26 +0500

    add kms_key_arn var

--- eks/variable.tf
+++ eks/variable.tf
@@ -6,3 +6,8 @@ variable "subnet_ids" {
   description = "a list of subent ids of VPC on which the cluster will operate"
   type        = map(string)
 }
+
+variable "kms_key_arn" {
+  description = "etcd secret keys encryption and decryption"
+  type        = string
+}
