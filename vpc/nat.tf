0e94825508e8e115c271e941f54098b8f6d2567f
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-03-31 11:56:37 +0500

    add nat gateway

---
+++ vpc/nat.tf
@@ -0,0 +1,16 @@
+resource "aws_eip" "nat_eip" {
+  tags = {
+    Project = "${local.project_name}"
+    Name    = "NAT EIP"
+  }
+}
+
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public.id
+
+  tags = {
+    Project = "${local.project_name}"
+    Name    = "NAT Gateway"
+  }
+}
