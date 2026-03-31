de354cfa86fa8f0b18fff984a14604f49ba03640
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-03-31 11:56:52 +0500

    add locals

---
+++ vpc/locals.tf
@@ -0,0 +1,7 @@
+locals {
+  region              = "eu-central-1"
+  project_name        = "epam-eks-hw"
+  vpc_cidr_block      = "10.0.0.0/16"
+  public_subnet_cidr  = "10.0.1.0/24"
+  private_subnet_cidr = "10.0.2.0/24"
+}
