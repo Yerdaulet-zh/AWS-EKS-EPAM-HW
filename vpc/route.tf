0756076c734cbea89acc8ccbad011c566cd7912f
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-03-31 12:10:57 +0500

    Add the second public subent to the public route table

--- vpc/route.tf
+++ vpc/route.tf
@@ -24,8 +24,13 @@ resource "aws_route_table" "private" {
   }
 }
 
-resource "aws_route_table_association" "public_route_table_association" {
-  subnet_id      = aws_subnet.public.id
+resource "aws_route_table_association" "public_route_table_association_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "public_route_table_association_2" {
+  subnet_id      = aws_subnet.public_2.id
   route_table_id = aws_route_table.public_route_table.id
 }
 
