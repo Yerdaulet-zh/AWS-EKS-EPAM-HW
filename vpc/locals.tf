630dd5734d7e6b1a6debfbccde871498ab86d9e9
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-04-01 11:01:55 +0500

    change the cidr block of all subnets

--- vpc/locals.tf
+++ vpc/locals.tf
@@ -2,8 +2,8 @@ locals {
   region               = "eu-central-1"
   project_name         = "epam-eks-hw"
   vpc_cidr_block       = "10.0.0.0/16"
-  public_subnet_cidr1  = "10.0.1.0/24"
-  public_subnet_cidr2  = "10.0.3.0/24"
-  private_subnet_cidr1 = "10.0.2.0/24"
-  private_subnet_cidr2 = "10.0.4.0/24"
+  public_subnet_cidr1  = "10.0.5.0/24"
+  public_subnet_cidr2  = "10.0.6.0/24"
+  private_subnet_cidr1 = "10.0.7.0/24"
+  private_subnet_cidr2 = "10.0.8.0/24"
 }
