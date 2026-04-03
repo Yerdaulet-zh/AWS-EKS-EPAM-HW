4a61c7b75754c9fac8fc3cf288fd6b9e817c6e55
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-04-03 11:37:16 +0500

    add sso_admin role

--- kms/data.tf
+++ kms/data.tf
@@ -3,3 +3,7 @@ data "aws_region" "current" {}
 data "aws_caller_identity" "current" {}
 
 data "aws_organizations_organization" "current" {}
+
+data "aws_iam_role" "sso_admin" {
+  name = "AWSReservedSSO_AdministratorAccess_3586681a9dcf4676"
+}
