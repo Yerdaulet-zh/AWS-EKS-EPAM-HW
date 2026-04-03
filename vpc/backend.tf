85110422bb79d903d90d7157fce031e3a0fe18e2
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-04-03 11:08:53 +0500

    bucket name and key change

--- vpc/backend.tf
+++ vpc/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket       = "shapagat-medeu-terraform-states"
-    key          = "second/eks-epam-hw/terraform.tfstate"
+    bucket       = "epam-terrform-state-bucket"
+    key          = "eks-epam-hw/vpc/terraform.tfstate"
     region       = "eu-central-1"
     encrypt      = true
     use_lockfile = true
