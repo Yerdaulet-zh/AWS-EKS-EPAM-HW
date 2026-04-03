37609d1e42c2ae935c0d006dca130ab419a85e89
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-04-03 11:04:39 +0500

    add and comment chache nodes, comment gpu nodes

--- eks/aws_eks_node_group.tf
+++ eks/aws_eks_node_group.tf
@@ -34,28 +34,63 @@ resource "aws_eks_node_group" "general_purpose_nodes" {
   ]
 }
 
-resource "aws_eks_node_group" "nvidia_gpu_nodes" {
-  cluster_name    = aws_eks_cluster.main.name
-  node_group_name = "nvidia-gpu-nodes"
-  node_role_arn   = aws_iam_role.eks_node_group_general_purpose_role.arn
-  subnet_ids      = values(var.private_subnets)
+# resource "aws_eks_node_group" "cache_nodes" {
+#   cluster_name    = aws_eks_cluster.main.name
+#   node_group_name = "cache-nodes"
+#   node_role_arn   = aws_iam_role.eks_node_group_general_purpose_role.arn
+#   subnet_ids      = values(var.private_subnets)
 
-  ami_type       = "AL2023_x86_64_NVIDIA" # "AL2023_ARM_64_NVIDIA"
-  instance_types = ["g6.xlarge"]
+#   ami_type        = "AL2023_ARM_64_STANDARD" # "AL2023_x86_64_STANDARD"
+#   instance_types  = ["t4g.medium"]
+#   release_version = nonsensitive(data.aws_ssm_parameter.eks_ami_amazon_linux_2023.value)
 
-  scaling_config {
-    desired_size = 1
-    max_size     = 2
-    min_size     = 1
-  }
+#   scaling_config {
+#     desired_size = 6
+#     max_size     = 6
+#     min_size     = 6
+#   }
 
-  release_version = nonsensitive(data.aws_ssm_parameter.eks_ami_nvidia_amazon_linux_2023.value)
+#   taint {
+#     key    = "dedicated"
+#     value  = "cache"
+#     effect = "NO_SCHEDULE"
+#   }
 
-  depends_on = [
-    aws_iam_role_policy_attachment.amazon_eks_worker_node_policy,
-    aws_iam_role_policy_attachment.amazon_eks_cni_policy,
-    aws_iam_role_policy_attachment.amazon_ecr_readonly,
-    aws_iam_role_policy_attachment.amazon_ssm_managed_instance_core,
-    aws_iam_role_policy_attachment.cloudwatch_agent_server_policy
-  ]
-}
+#   labels = {
+#     role = "cache"
+#   }
+
+#   depends_on = [
+#     aws_iam_role_policy_attachment.amazon_eks_worker_node_policy,
+#     aws_iam_role_policy_attachment.amazon_eks_cni_policy,
+#     aws_iam_role_policy_attachment.amazon_ecr_readonly,
+#     aws_iam_role_policy_attachment.amazon_ssm_managed_instance_core,
+#     aws_iam_role_policy_attachment.cloudwatch_agent_server_policy
+#   ]
+# }
+
+# resource "aws_eks_node_group" "nvidia_gpu_nodes" {
+#   cluster_name    = aws_eks_cluster.main.name
+#   node_group_name = "nvidia-gpu-nodes"
+#   node_role_arn   = aws_iam_role.eks_node_group_general_purpose_role.arn
+#   subnet_ids      = values(var.private_subnets)
+
+#   ami_type       = "AL2023_x86_64_NVIDIA" # "AL2023_ARM_64_NVIDIA"
+#   instance_types = ["g6.xlarge"]
+
+#   scaling_config {
+#     desired_size = 1
+#     max_size     = 2
+#     min_size     = 1
+#   }
+
+#   release_version = nonsensitive(data.aws_ssm_parameter.eks_ami_nvidia_amazon_linux_2023.value)
+
+#   depends_on = [
+#     aws_iam_role_policy_attachment.amazon_eks_worker_node_policy,
+#     aws_iam_role_policy_attachment.amazon_eks_cni_policy,
+#     aws_iam_role_policy_attachment.amazon_ecr_readonly,
+#     aws_iam_role_policy_attachment.amazon_ssm_managed_instance_core,
+#     aws_iam_role_policy_attachment.cloudwatch_agent_server_policy
+#   ]
+# }
