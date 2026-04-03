a76ab37243bee9aab9d88761a9d31fc47dd48cc9
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-04-03 15:29:04 +0500

    creation of ECR repository

---
+++ ecr/ecr.tf
@@ -0,0 +1,37 @@
+module "ecr" {
+  source  = "terraform-aws-modules/ecr/aws"
+  version = "~> 2.0"
+
+  repository_name = "epam-hw-repository"
+
+  repository_image_tag_mutability = "IMMUTABLE"
+
+  repository_image_scan_on_push = true
+  repository_encryption_type    = "KMS"
+  repository_kms_key            = var.kms_key_arn # Optional: defaults to AWS managed key
+
+  repository_read_write_access_arns = [data.aws_caller_identity.this.arn]
+
+  repository_lifecycle_policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Keep last 30 images",
+        selection = {
+          tagStatus     = "tagged",
+          tagPrefixList = ["v"],
+          countType     = "imageCountMoreThan",
+          countNumber   = 30
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
