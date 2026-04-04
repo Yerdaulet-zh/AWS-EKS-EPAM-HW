e7b838bea438eb097bf0d0f27ef15e57e63f94e3
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-04-04 13:44:39 +0500

    add helm provider and http

--- eks/provider.tf
+++ eks/provider.tf
@@ -6,10 +6,31 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 6.28"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 3.0"
+    }
   }
+
 }
 
 provider "aws" {
   region  = "eu-central-1"
   profile = "895587011312_AdministratorAccess"
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = aws_eks_cluster.main.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.main.token
+  }
+}
+
+data "aws_eks_cluster_auth" "main" {
+  name = aws_eks_cluster.main.name
 }
