3938729c06639fce11188011ae95dee8d6955877
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-04-07 23:04:13 +0500

    update variable to match with the format which from vpc part

--- eks/variable.tf
+++ eks/variable.tf
@@ -2,9 +2,10 @@ variable "cluster_name" {
   type = string
 }
 
-variable "subnet_ids" {
-  description = "a map of subnet ids of VPC on which the cluster will operate"
-  type        = map(string)
+variable "vpc_id" {
+  description = "VPC ID for the EKS cluster (deprecated - sourced from vpc remote state)"
+  type        = string
+  default     = ""
 }
 
 variable "kms_key_arn" {
@@ -12,12 +13,20 @@ variable "kms_key_arn" {
   type        = string
 }
 
-variable "public_subnets" {
-  description = "A map of public subnet IDs"
+variable "subnet_ids" {
+  description = "a map of subnet ids of VPC on which the cluster will operate (deprecated - sourced from vpc remote state)"
   type        = map(string)
+  default     = {}
+}
+
+variable "public_subnets" {
+  description = "A map of public subnet IDs (deprecated - sourced from vpc remote state)"
+  type        = map(string)
+  default     = {}
 }
 
 variable "private_subnets" {
-  description = "A map of private subnet IDs"
+  description = "A map of private subnet IDs (deprecated - sourced from vpc remote state)"
   type        = map(string)
+  default     = {}
 }
