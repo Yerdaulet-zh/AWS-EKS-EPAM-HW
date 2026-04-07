2a69ef788f9225c9159b7df4025b151ce54cfb56
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-04-07 21:05:46 +0500

    change the scaling config of cache nodes to 1. add node group for monitoring.

--- eks/aws_eks_node_group.tf
+++ eks/aws_eks_node_group.tf
@@ -57,9 +57,9 @@ resource "aws_eks_node_group" "cache_nodes" {
   release_version = nonsensitive(data.aws_ssm_parameter.eks_ami_amazon_linux_2023.value)
 
   scaling_config {
-    desired_size = 4
-    max_size     = 4
-    min_size     = 4
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
   }
 
   taint {
@@ -83,6 +83,42 @@ resource "aws_eks_node_group" "cache_nodes" {
 
   tags = {
     Name = "${var.cluster_name}-general-purpose-node"
+  }
+}
+
+resource "aws_eks_node_group" "monitoring_nodes" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = "monitoring-nodes"
+  node_role_arn   = aws_iam_role.eks_node_group_general_purpose_role.arn
+
+  subnet_ids = values(var.private_subnets)
+
+  capacity_type   = "ON_DEMAND"
+  instance_types  = ["t4g.medium"]
+  ami_type        = "AL2023_ARM_64_STANDARD"
+  release_version = nonsensitive(data.aws_ssm_parameter.eks_ami_amazon_linux_2023.value)
+
+  scaling_config {
+    desired_size = 1
+    min_size     = 1
+    max_size     = 1
+  }
+
+  labels = {
+    role                             = "monitoring"
+    "node-role.kubernetes.io/worker" = "worker"
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.amazon_eks_worker_node_policy,
+    aws_iam_role_policy_attachment.amazon_eks_cni_policy,
+    aws_iam_role_policy_attachment.amazon_ecr_readonly,
+    aws_iam_role_policy_attachment.amazon_ssm_managed_instance_core,
+    aws_iam_role_policy_attachment.cloudwatch_agent_server_policy
+  ]
+
+  tags = {
+    Name = "${var.cluster_name}-monitoring-node"
   }
 }
 
