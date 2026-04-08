603db251b606d8aa5d5097a2ca237e485fd697a1
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-04-08 13:46:25 +0500

    Diable NAT

--- vpc/route.tf
+++ vpc/route.tf
@@ -7,22 +7,24 @@ resource "aws_route_table" "public_route_table" {
   }
 
   tags = {
+    Name    = "Public Route Table"
     Project = "${local.project_name}"
   }
 }
 
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.vpc.id
+# resource "aws_route_table" "private" {
+#   vpc_id = aws_vpc.vpc.id
 
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat_gateway.id
-  }
+#   route {
+#     cidr_block     = "0.0.0.0/0"
+#     nat_gateway_id = aws_nat_gateway.nat_gateway.id
+#   }
 
-  tags = {
-    Name = "Private Route Table"
-  }
-}
+#   tags = {
+#     Name    = "Private Route Table"
+#     Project = "${local.project_name}"
+#   }
+# }
 
 resource "aws_route_table_association" "public_route_table_association_1" {
   subnet_id      = aws_subnet.public_1a.id
@@ -34,12 +36,12 @@ resource "aws_route_table_association" "public_route_table_association_2" {
   route_table_id = aws_route_table.public_route_table.id
 }
 
-resource "aws_route_table_association" "private_rt_association_1" {
-  subnet_id      = aws_subnet.private_1a.id
-  route_table_id = aws_route_table.private.id
-}
+# resource "aws_route_table_association" "private_rt_association_1" {
+#   subnet_id      = aws_subnet.private_1a.id
+#   route_table_id = aws_route_table.private.id
+# }
 
-resource "aws_route_table_association" "private_rt_association_2" {
-  subnet_id      = aws_subnet.private_2b.id
-  route_table_id = aws_route_table.private.id
-}
+# resource "aws_route_table_association" "private_rt_association_2" {
+#   subnet_id      = aws_subnet.private_2b.id
+#   route_table_id = aws_route_table.private.id
+# }
