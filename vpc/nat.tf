8d50db75429015f5128714b9e7d4b78c55d1013e
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-04-08 18:52:25 +0500

    allow nat gateway for private subnets

--- vpc/nat.tf
+++ vpc/nat.tf
@@ -1,16 +1,16 @@
-# resource "aws_eip" "nat_eip" {
-#   tags = {
-#     Project = "${local.project_name}"
-#     Name    = "NAT EIP"
-#   }
-# }
+resource "aws_eip" "nat_eip" {
+  tags = {
+    Project = "${local.project_name}"
+    Name    = "NAT EIP"
+  }
+}
 
-# resource "aws_nat_gateway" "nat_gateway" {
-#   allocation_id = aws_eip.nat_eip.id
-#   subnet_id     = aws_subnet.public_1a.id
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_1a.id
 
-#   tags = {
-#     Project = "${local.project_name}"
-#     Name    = "NAT Gateway"
-#   }
-# }
+  tags = {
+    Project = "${local.project_name}"
+    Name    = "NAT Gateway"
+  }
+}
