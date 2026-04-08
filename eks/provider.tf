03bad1ec330dbff4ebb991ed70e4fe3e9ce39cd1
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-04-08 13:47:10 +0500

    Disable helm provider

--- eks/provider.tf
+++ eks/provider.tf
@@ -23,14 +23,14 @@ provider "aws" {
   profile = "895587011312_AdministratorAccess"
 }
 
-provider "helm" {
-  kubernetes {
-    host                   = aws_eks_cluster.main.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.main.token
-  }
-}
+# provider "helm" {
+#   kubernetes {
+#     host                   = aws_eks_cluster.main.endpoint
+#     cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+#     token                  = data.aws_eks_cluster_auth.main.token
+#   }
+# }
 
-data "aws_eks_cluster_auth" "main" {
-  name = aws_eks_cluster.main.name
-}
+# data "aws_eks_cluster_auth" "main" {
+#   name = aws_eks_cluster.main.name
+# }
