cc1d4635f7599ee88d0984a35e1f721a2a77a8b1
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-04-09 15:58:02 +0500

    add route 53 domains and oidc url

--- eks/locals.tf
+++ eks/locals.tf
@@ -1,3 +1,8 @@
 locals {
   region = "eu-central-1"
+  domains = [
+    "imon.work",
+    "imon.academy"
+  ]
+  oidc_url = replace(aws_eks_cluster.main.identity[0].oidc[0].issuer, "https://", "")
 }
